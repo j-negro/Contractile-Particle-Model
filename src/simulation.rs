2c15fe3df0226764759bf31f7a06fc95e11cebdf
Juan Negro <jnegro@itba.edu.ar>
2023-05-29 20:23:17 -0300

    Removed CIM and fixed initial particle generation

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,27 +1,26 @@
-use neighbors::{cell_index_method::CellIndexMethod, NeighborMethod, Particle as MethodParticle};
+use neighbors::Particle as MethodParticle;
 use rand::Rng;
 
 use crate::{
-    constants::{PARTICLE_COUNT, SIMULATION_LENGHT},
+    constants::{MIN_PARTICLE_RADIUS, PARTICLE_COUNT, SIMULATION_LENGHT},
     particle::Particle,
 };
 
 pub struct Simulation {
     pub particles: Vec<Particle>,
-    neighbors_method: CellIndexMethod<Particle>,
 }
 
 impl Simulation {
     pub fn new() -> Simulation {
         let mut particles = Vec::with_capacity(PARTICLE_COUNT);
         let mut rng = rand::thread_rng();
-        // TODO: Validate issue with interaction range too small.
-        let neighbors_method = CellIndexMethod::new(SIMULATION_LENGHT + 3.0, Some(23), 0.0, false);
 
         for i in 0..PARTICLE_COUNT {
             loop {
-                let x = rng.gen_range(0.0..=SIMULATION_LENGHT);
-                let y = rng.gen_range(0.0..=SIMULATION_LENGHT);
+                let x =
+                    rng.gen_range(MIN_PARTICLE_RADIUS..=(SIMULATION_LENGHT - MIN_PARTICLE_RADIUS));
+                let y =
+                    rng.gen_range(MIN_PARTICLE_RADIUS..=(SIMULATION_LENGHT - MIN_PARTICLE_RADIUS));
                 let particle = Particle::new(i, x, y);
 
                 if particles.iter().any(|p| particle.is_colliding(&p)) {
@@ -33,41 +32,53 @@ impl Simulation {
             }
         }
 
-        Simulation {
-            particles,
-            neighbors_method,
-        }
+        Simulation { particles }
     }
 
     pub fn run(&mut self, steps: usize) {
         for _ in 0..steps {
-            self.neighbors_method.set_particles(self.particles.clone());
-            let neighbors = self.neighbors_method.calculate_neighbors();
+            let mut neighbors = Vec::new();
+
+            for particle in &self.particles {
+                neighbors.push((particle.id, Vec::new()));
+            }
+
+            for i in 0..self.particles.len() {
+                for j in i + 1..self.particles.len() {
+                    if self.particles[i].is_colliding(&self.particles[j]) {
+                        let particle_coords = self.particles[i].get_coordinates();
+                        let neighbor_coords = self.particles[j].get_coordinates();
+
+                        neighbors[i].1.push(neighbor_coords);
+                        neighbors[j].1.push(particle_coords);
+                    }
+                }
+            }
+
             let mut to_remove = Vec::new();
 
             for (idx, particle) in self.particles.iter_mut().enumerate() {
                 let mut collisions_points = particle.check_wall_collisions();
 
-                if neighbors[particle.id].is_empty() && collisions_points.is_empty() {
-                    particle.step_desired();
+                let neighbors_coords = neighbors[idx].1.clone();
+
+                if neighbors_coords.is_empty() && collisions_points.is_empty() {
+                    particle.update_desired();
                 } else {
-                    collisions_points.append(
-                        &mut neighbors[particle.id]
-                            .iter()
-                            .map(|p| p.get_coordinates())
-                            .collect(),
-                    );
-                    particle.step_escape(&collisions_points);
-                }
+                    collisions_points.extend(neighbors_coords);
 
+                    particle.update_escape(&collisions_points);
+                }
+            }
+
+            for particle in self.particles.iter_mut() {
+                particle.step();
                 if particle.check_reached_target() {
-                    to_remove.push(idx);
+                    to_remove.push(particle.id);
                 }
             }
 
-            for idx in to_remove {
-                self.particles.remove(idx);
-            }
+            self.particles.retain(|p| !to_remove.contains(&p.id));
         }
     }
 }
