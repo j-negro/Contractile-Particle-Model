4127da5ee40540cfb8561a6a8bddca1cc93c05c0
Juan Negro <jnegro@itba.edu.ar>
2023-05-29 11:35:43 -0300

    Add step methods to Particle

--- src/particle.rs
+++ src/particle.rs
@@ -1,4 +1,7 @@
-use crate::constants::{MIN_PARTICLE_RADIUS, TARGET_LEFT_X, TARGET_RIGHT_X, TARGET_SIZE};
+use crate::constants::{
+    calculate_desired_velocity, MAX_DESIRED_VELOCITY, MAX_PARTICLE_RADIUS, MIN_PARTICLE_RADIUS,
+    RADIUS_INCREMENT, TARGET_LEFT_X, TARGET_RIGHT_X, TARGET_SIZE,
+};
 use neighbors::Particle as MethodParticle;
 use rand::Rng;
 use std::hash::Hash;
@@ -38,7 +41,7 @@ impl Particle {
         self.distance(other) <= self.radius + other.radius
     }
 
-    pub fn get_target(x_coordinate: f64) -> (f64, f64) {
+    fn get_target(x_coordinate: f64) -> (f64, f64) {
         let x_min_target = TARGET_LEFT_X + 0.2 * TARGET_SIZE;
         let x_max_target = TARGET_RIGHT_X + 0.8 * TARGET_SIZE;
         if x_coordinate < x_min_target || x_coordinate > x_max_target {
@@ -46,6 +49,48 @@ impl Particle {
             return (target_x, 0.0);
         }
         (x_coordinate, 0.0)
+    }
+
+    fn step(&mut self) {
+        self.x += self.vx;
+        self.y += self.vy;
+    }
+
+    pub fn step_escape(&mut self, collision_points: &[(f64, f64)]) {
+        self.radius = MIN_PARTICLE_RADIUS;
+
+        let mut collision_vector = (0.0, 0.0);
+
+        for (x, y) in collision_points {
+            let diff = (self.x - x, self.y - y);
+            let norm = (diff.0.powi(2) + diff.1.powi(2)).sqrt();
+
+            collision_vector.0 += diff.0 / norm;
+            collision_vector.1 += diff.1 / norm;
+        }
+
+        let norm = (collision_vector.0.powi(2) + collision_vector.1.powi(2)).sqrt();
+
+        self.vx = MAX_DESIRED_VELOCITY * collision_vector.0 / norm;
+        self.vy = MAX_DESIRED_VELOCITY * collision_vector.1 / norm;
+
+        self.step();
+    }
+
+    pub fn step_desired(&mut self) {
+        if self.radius < MAX_PARTICLE_RADIUS {
+            self.radius += RADIUS_INCREMENT;
+        }
+
+        let desired_velocity = calculate_desired_velocity(self.radius);
+
+        let target_direction = (self.target.0 - self.x, self.target.1 - self.y);
+        let target_norm = (target_direction.0.powi(2) + target_direction.1.powi(2)).sqrt();
+
+        self.vx = desired_velocity * target_direction.0 / target_norm;
+        self.vy = desired_velocity * target_direction.1 / target_norm;
+
+        self.step();
     }
 }
 
