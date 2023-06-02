3b270eb2fd210962914b7ad3f447ca1b580853cc
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-06-02 17:30:05 -0300

    add python script to run simulation

---
+++ analysis/run_variable_target.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+
+import numpy as np
+
+RUNS = 10
+
+
+RESULTS_PATH = "./analysis/data/"
+
+DATA = [(1.2, 200), (1.8, 260), (2.4, 320), (3.0, 380)]
+
+os.makedirs(RESULTS_PATH, exist_ok=True)
+for d, N in DATA:
+    print(f"Starting runs d {d} and N {N}")
+    for run_idx in range(RUNS):
+        subprocess.run(
+            [
+                "./target/release/contractile-particle-model",
+                "-p",
+                str(N),
+                "-t",
+                str(d),
+                "--data-output-path",
+                RESULTS_PATH + f"{d}_{N}_{run_idx}.txt",
+            ],
+            capture_output=True,
+            text=True,
+        )
