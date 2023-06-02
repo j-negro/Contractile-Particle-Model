1a643eac3015c4c38590976f3160822dcdf3f4c1
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-06-02 12:00:48 -0300

    fix xyz file

--- src/io.rs
+++ src/io.rs
@@ -15,7 +15,7 @@ const CORNERS: [(f64, f64); 4] = [
 pub fn output_simulation(file: &File, particles: &Vec<Particle>, target: &Target) -> Result<()> {
     let mut writer = BufWriter::new(file);
 
-    let particle_count = particles.len() + CORNERS.len();
+    let particle_count = particles.len() + CORNERS.len() + 2;
     writeln!(writer, "{particle_count}")?;
     writeln!(writer, "Properties=pos:R:2:velo:R:2:radius:R:1 pbc=\"F F\"",)?;
 
@@ -42,7 +42,6 @@ pub fn output_simulation(file: &File, particles: &Vec<Particle>, target: &Target
 
     // NOTE: Write the target
     writeln!(writer, "{:.12} {:.12} 0 0 0.05", target.left, 0.0)?;
-
     writeln!(writer, "{:.12} {:.12} 0 0 0.05", target.right, 0.0)?;
 
     Ok(())
